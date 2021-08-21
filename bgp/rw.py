4ab93af29f25d998dc9c31737b67da28b51338a0
izumin-baal <milton96.engineer@gmail.com>
2021-08-22 03:48:21 +0900

    :bug: おかしいところを修正

--- bgp/rw.py
+++ bgp/rw.py
@@ -1,22 +1,34 @@
 import pandas as pd
 from pandas.core.frame import DataFrame
 
-def into_bgptable(prefixaddr, next_hop=None, as_path=None, community=None):
+debug = True
+
+def into_bgptable(addrprefix, next_hop=None, as_path=None, community=None):
     df = pd.read_csv('bgp_table.csv', dtype=object ,encoding='utf_8')
     flag = True
-    address, prefix = prefixaddr.split('/')
-    for i in df.itertuples():
+    address, prefix = addrprefix.split('/')
+    for cnt,i in enumerate(df.itertuples()):
         p = i.address +  '/' + i.prefix
-        if prefixaddr == p:
+        if addrprefix == p:
             # 書き換え処理
-            df.iloc[0] = [address, prefix, next_hop, as_path, community]
-            df.to_csv('bgp_table.csv', mode='w', index=False)
+            df.at[cnt, 'address'] = address
+            df.at[cnt, 'prefix'] = prefix
+            df.at[cnt, 'next_hop'] = next_hop
+            df.at[cnt, 'as_path'] = as_path
+            df.at[cnt, 'community'] = community
             flag = False
+            df.to_csv('bgp_table.csv', mode='w', index=False)
+            print('match')
             break
     if flag:
         #新規追加
         data = pd.DataFrame([[address, prefix, next_hop, as_path, community]], columns=['address', 'prefix', 'next_hop', 'as_path', 'community'])
         data.to_csv('bgp_table.csv', mode='a', index=False, header=False)
+    if debug:
+        print("######### BGP_TABLE.csv #########")
+        df = pd.read_csv('bgp_table.csv', dtype=object ,encoding='utf_8')
+        print(df)
+        print("#################################")
 
 def del_from_bgptable(addrprefix):
     df = pd.read_csv('bgp_table.csv', dtype=object ,encoding='utf_8')
@@ -28,3 +40,7 @@ def del_from_bgptable(addrprefix):
             df.drop(cnt,inplace=True)
             df.to_csv('bgp_table.csv', mode='w', index=False)
             break
+    if debug:
+        print("######### BGP_TABLE.csv #########")
+        print(df)
+        print("#################################")
--- practice_socket/pandas_test/rw.py
+++ practice_socket/pandas_test/rw.py
@@ -1,38 +1,46 @@
 import pandas as pd
 from pandas.core.frame import DataFrame
 
-def into_bgptable(prefixaddr, next_hop=None, as_path=None, community=None):
+debug = True
+
+def into_bgptable(addrprefix, next_hop=None, as_path=None, community=None):
     df = pd.read_csv('bgp_table.csv', dtype=object ,encoding='utf_8')
     flag = True
-    address, prefix = prefixaddr.split('/')
-    for i in df.itertuples():
+    address, prefix = addrprefix.split('/')
+    for cnt,i in enumerate(df.itertuples()):
         p = i.address +  '/' + i.prefix
-        if prefixaddr == p:
+        if addrprefix == p:
             # 書き換え処理
-            df.iloc[0] = [address, prefix, next_hop, as_path, community]
-            df.to_csv('bgp_table.csv', mode='w', index=False)
+            df.at[cnt, 'address'] = address
+            df.at[cnt, 'prefix'] = prefix
+            df.at[cnt, 'next_hop'] = next_hop
+            df.at[cnt, 'as_path'] = as_path
+            df.at[cnt, 'community'] = community
             flag = False
+            df.to_csv('bgp_table.csv', mode='w', index=False)
+            print('match')
             break
     if flag:
         #新規追加
         data = pd.DataFrame([[address, prefix, next_hop, as_path, community]], columns=['address', 'prefix', 'next_hop', 'as_path', 'community'])
         data.to_csv('bgp_table.csv', mode='a', index=False, header=False)
+    if debug:
+        print("######### BGP_TABLE.csv #########")
+        df = pd.read_csv('bgp_table.csv', dtype=object ,encoding='utf_8')
+        print(df)
+        print("#################################")
 
 def del_from_bgptable(addrprefix):
-    print('del()')
     df = pd.read_csv('bgp_table.csv', dtype=object ,encoding='utf_8')
     address, prefix = addrprefix.split('/')
     for cnt,i in enumerate(df.itertuples()):
         p = i.address +  '/' + i.prefix
         if addrprefix == p:
             # 削除処理
-            print(cnt)
             df.drop(cnt,inplace=True)
-            print(df)
             df.to_csv('bgp_table.csv', mode='w', index=False)
-            print('match')
             break
-
-
-if __name__ == "__main__":
-    into_bgptable('1.1.1.1/31',1)
+    if debug:
+        print("######### BGP_TABLE.csv #########")
+        print(df)
+        print("#################################")
